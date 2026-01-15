c7f83d6df3c7dd4bf2700b2d0fd0354c26890266
shruti-k-stack <shrutikhandagiri2@gmail.com>
2026-01-15 20:52:21 +0530

    'Started with setup'

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h1 className="text-3xl font-bold underline text-green-800">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
